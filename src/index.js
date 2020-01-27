00f4170c24535ba3a838db41302e03012e175da2
Dakota Dunn <dakotadunn84@gmail.com>
2020-01-26 22:02:01 -0800

    Add support for user profile images

--- src/index.js
+++ src/index.js
@@ -9,18 +9,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -28,17 +16,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('5e292da94512b161308c9513')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5e292c9e9857e268a4db230a')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-// main()
