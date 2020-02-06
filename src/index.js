72ff4812db99727ac2ba428a88e8d537526fb344
Dakota Dunn <dakotadunn84@gmail.com>
2020-02-06 00:08:27 -0800

    Add Jest testing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 // use this to startup mongodb server: /DDevStuff/mongoDB/mongodb/bin/mongod.exe --dbpath=/DDevStuff/mongoDB/mongodb
 // use this to startup local host: npm run dev
 
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
